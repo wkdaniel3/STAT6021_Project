f943ebf4039dbd90b4c359662fcc280eb0cbcfb6
Daniel <DanielW20@darden.virginia.edu>
2018-12-01 10:49:50 -0500

    Adding Data Analysis R File

---
+++ dataAnalysis.R
@@ -0,0 +1,8 @@
+## Project Analysis ##
+
+# Set WD for project
+repoLoc <- ("/Repositories/STAT6021_project")
+setwd(repoLoc)
+
+#load in training data
+train.data <- read.csv("traindata_subset.csv", header = TRUE)
